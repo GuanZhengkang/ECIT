9ca164561d81e0e2907734d8cd6b2ef14c4b36a7
Gzk <19906039211@163.com>
2025-01-07 17:40:03 +0800

    ChooseEnsemble.ipynb test

--- ecit/epvalue.py
+++ ecit/epvalue.py
@@ -12,33 +12,39 @@ def p_stable(p_list:List[float],
 
 
 
-def p_alpha02(p_list:List[float]):
-    return p_stable(p_list, alpha = 0.2)
+def p_alpha025(p_list:List[float]):
+    return p_stable(p_list, alpha = 0.25)
 
 
 def p_alpha05(p_list:List[float]):
     return p_stable(p_list, alpha = 0.5)
 
 
-def p_alpha08(p_list:List[float]):
-    return p_stable(p_list, alpha = 0.8)
+def p_alpha075(p_list:List[float]):
+    return p_stable(p_list, alpha = 0.75)
 
+
+def p_alpha1(p_list:List[float]):
+    return p_stable(p_list, alpha = 1)
 
 def p_cauchy(p_list:List[float]):
     return p_stable(p_list, alpha = 1)
 
 
-def p_alpha12(p_list:List[float]):
-    return p_stable(p_list, alpha = 1.2)
+def p_alpha125(p_list:List[float]):
+    return p_stable(p_list, alpha = 1.25)
 
 
 def p_alpha15(p_list:List[float]):
     return p_stable(p_list, alpha = 1.5)
 
 
-def p_alpha18(p_list:List[float]):
-    return p_stable(p_list, alpha = 1.8)
+def p_alpha175(p_list:List[float]):
+    return p_stable(p_list, alpha = 1.75)
 
+
+def p_alpha2(p_list:List[float]):
+    return p_stable(p_list, alpha = 2)
 
 def p_gaussian(p_list:List[float]):
     return p_stable(p_list, alpha = 2)
